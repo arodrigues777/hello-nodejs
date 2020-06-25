73357c30837c79dd279b8ae6493817198dfdbc66
Anderson Rodrigues <anderson.rodrigues@outlook.jp>
2020-06-25 10:07:40 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const http = require('http');
+var express = require('express');
+var app = express();
 
 //const hostname = '127.0.0.1';
 //const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+app.get('/', function(req, res) {
   res.end('Hello World');
 });
 
-server.listen(80, function () {
+app.listen(process.env.PORT || 80, function () {
   console.log('Server running at');
 });
